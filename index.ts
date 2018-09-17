6f35d205f1b26c797ce4489cfabb81e8e114f683
Vincent Bédard <vedard@gmail.com>
2018-09-17 13:14:31 -0400

    Change map to object

--- index.ts
+++ index.ts
@@ -5,62 +5,73 @@ export class Statement {
     public Text: string = "";
     public Values: any[] = [];
 
-    public join(table: string, fields: Map<string, string>, type: string = "INNER") {
-        let expression = Array.from(fields.entries(), (v, k) => { return `${v[0]} = ${v[1]}` });
+    public join(table: string, fields: Object, type: string = "INNER") {
+        let expression = Object.entries(fields).map((v, k) => `${v[0]} = ${v[1]}`);
         this.Text += ` ${type} JOIN ${table} ON ${expression.join(' AND ')}`;
         return this;
     }
 
-    public where(fields: Map<string, string>) {
-        let expression = Array.from(fields.keys(), (v, k) => { return `${v} = $${k + this.Values.length + 1}` });
-        this.Values = this.Values.concat(Array.from(fields.values()));
+    public where(fields: Object) {
+        let expression = Object.keys(fields).map((v, k) => `${v} = $${k + this.Values.length + 1}`);
+        this.Values = this.Values.concat(Object.values(fields));
         this.Text += ` WHERE ${expression.join(' AND ')}`;
         return this;
     }
 
-    public orderby(fields: string[]){ 
-        this.Text += ` ORDER BY ${fields.join(', ')}`;
+    public groubby(columns: string[]) {
+        this.Text += ` GROUB BY ${columns.join(', ')}`;
         return this;
-
     }
 
-    public returning(fields: string[]) {
-        this.Text += ` RETURNING ${fields.join(', ')}`;
+    public having(fields: Object){
+        let expression = Object.keys(fields).map((v, k) => `${v} = $${k + this.Values.length + 1}`);
+        this.Values = this.Values.concat(Object.values(fields));
+        this.Text += ` HAVING ${expression.join(' AND ')}`;
+        return this;
+    }
+
+    public orderby(columns: string[]) {
+        this.Text += ` ORDER BY ${columns.join(', ')}`;
+        return this;
+    }
+
+    public returning(columns: string[]) {
+        this.Text += ` RETURNING ${columns.join(', ')}`;
         return this;
     }
 }
 
-export function select(table: string, fields: string[] ) {
+export function select(table: string, columns: string[]) {
     let statement = new Statement();
     statement.Operation = "SELECT";
     statement.Table = table;
-    statement.Text = `SELECT ${fields.join(', ')} FROM ${table}`;
-    return statement; 
+    statement.Text = `SELECT ${columns.join(', ')} FROM ${table}`;
+    return statement;
 }
 
 
-export function insert(table: string, fields: Map<string, string>) {
-    let columns = Array.from(fields.keys());
-    let values = Array.from({length: fields.size}, (v, k)=>{return `$${k + 1}`;})
+export function insert(table: string, fields: Object) {
+    let columns = Object.keys(fields)
+    let values = Object.values(fields).map((v, k) => `$${k + 1}`);
     let statement = new Statement();
     statement.Operation = "INSERT";
     statement.Table = table;
-    statement.Values = Array.from(fields.values()); 
+    statement.Values = Object.values(fields);
     statement.Text = `INSERT INTO ${table} (${columns.join(", ")}) VALUES (${values.join(", ")})`;
     return statement;
 }
 
-export function update(table: string, fields: Map<string, string>, where_fields: Map<string, string>) {
-    let columns = Array.from(fields.keys(), (v, k) => { return `${v} = $${k + 1}` });
+export function update(table: string, fields: Object) {
+    let columns = Object.keys(fields).map((v, k) => `${v} = $${k + 1}`);
     let statement = new Statement();
     statement.Operation = "UPDATE";
     statement.Table = table;
-    statement.Values = Array.from(fields.values()); 
+    statement.Values = Object.values(fields);
     statement.Text = `UPDATE ${table} SET ${columns.join(", ")}`;
     return statement;
 }
 
-export function deletes(table: string): Statement{
+export function deletes(table: string): Statement {
     let statement = new Statement();
     statement.Operation = "DELETE";
     statement.Table = table;
--- test/test.js
+++ test/test.js
@@ -2,29 +2,31 @@ const assert = require('assert');
 const sql = require('../dist');
 
 describe('SqlQueryGenerator', function () {
-    describe('select()', function(){
-        it('1 field', function(){
+    describe('select()', function () {
+        it('1 field', function () {
             let query = sql.select('table', ['id']);
             assert.strictEqual(query.Text, "SELECT id FROM table");
         })
 
-        it('2 fields', function(){
+        it('2 fields', function () {
             let query = sql.select('table', ['id', 'name']);
             assert.strictEqual(query.Text, "SELECT id, name FROM table");
+            assert.deepStrictEqual(query.Values, []);
         })
 
-        it('with a join', function(){
-            let query = sql.select('table', ['id', 'name']).join('other_table', new Map([['table.id', 'other_table.id']]));
+        it('with a join', function () {
+            let query = sql.select('table', ['id', 'name']).join('other_table', { 'table.id': 'other_table.id' });
             assert.strictEqual(query.Text, "SELECT id, name FROM table INNER JOIN other_table ON table.id = other_table.id");
+            assert.deepStrictEqual(query.Values, []);
         })
 
-        it('with a where', function(){
-            let query = sql.select('table', ['id', 'name']).where(new Map([['id', 2]]))
+        it('with a where', function () {
+            let query = sql.select('table', ['id', 'name']).where({ id: 2 });
             assert.strictEqual(query.Text, "SELECT id, name FROM table WHERE id = $1");
             assert.deepStrictEqual(query.Values, [2]);
         })
 
-        it('with a order by', function(){
+        it('with a order by', function () {
             let query = sql.select('table', ['id', 'name']).orderby(["name"])
             assert.strictEqual(query.Text, "SELECT id, name FROM table ORDER BY name");
         })
@@ -32,40 +34,59 @@ describe('SqlQueryGenerator', function () {
 
     describe('insert()', function () {
         it('Single Value', function () {
-            let query = sql.insert("table", new Map([['f1', 2,]]));
+            let query = sql.insert("table", { f1: 2 });
             assert.strictEqual(query.Text, "INSERT INTO table (f1) VALUES ($1)");
             assert.deepStrictEqual(query.Values, [2]);
         });
         it('Double Value', function () {
-            let query = sql.insert("table", new Map([['f1', 2,], ['f2', 'string']]));
+            let query = sql.insert("table", { f1: 2, f2: 'string' });
             assert.strictEqual(query.Text, "INSERT INTO table (f1, f2) VALUES ($1, $2)");
             assert.deepStrictEqual(query.Values, [2, 'string']);
+        });
+
+        it('with returning clause', function () {
+            let query = sql.insert("table", { id: 1, title: "test" }).returning(["*"]);
+            assert.strictEqual(query.Text, "INSERT INTO table (id, title) VALUES ($1, $2) RETURNING *");
+            assert.deepStrictEqual(query.Values, [1, 'test']);
         });
     });
 
     describe('update()', function () {
         it('Single value', function () {
-            let query = sql.update("table", new Map([['f1', 2]]));
+            let query = sql.update("table", { f1: 2 });
             assert.strictEqual(query.Text, "UPDATE table SET f1 = $1");
             assert.deepStrictEqual(query.Values, [2]);
         });
 
         it('Double value', function () {
-            let query = sql.update("table", new Map([['f1', 2,], ['f2', 'string']]));
+            let query = sql.update("table", { f1: 2, f2: 'string' });
             assert.strictEqual(query.Text, "UPDATE table SET f1 = $1, f2 = $2");
             assert.deepStrictEqual(query.Values, [2, 'string']);
         });
 
         it('single where', function () {
-            let query = sql.update("table", new Map([['f1', 2,], ['f2', 'string']])).where(new Map([['f3', 'asdf',]]));
+            let query = sql.update("table", { f1: 2, f2: 'string' }).where({ f3: 'asdf' });
             assert.strictEqual(query.Text, "UPDATE table SET f1 = $1, f2 = $2 WHERE f3 = $3");
             assert.deepStrictEqual(query.Values, [2, 'string', 'asdf']);
         });
 
         it('Double where', function () {
-            let query = sql.update("table", new Map([['f1', 2,], ['f2', 'string']])).where(new Map([['f3', 'asdf',], ['f4', 'asdf']]));
+            let query = sql.update("table", { f1: 2, f2: 'string' }).where({ f3: 'asdf', f4: 'asdf' });
             assert.strictEqual(query.Text, "UPDATE table SET f1 = $1, f2 = $2 WHERE f3 = $3 AND f4 = $4");
             assert.deepStrictEqual(query.Values, [2, 'string', 'asdf', 'asdf']);
+        });
+    });
+
+    describe('delete()', function () {
+        it('Simple', function () {
+            let query = sql.deletes("table")
+            assert.strictEqual(query.Text, "DELETE FROM table");
+            assert.deepStrictEqual(query.Values, []);
+        });
+        it('with a where', function () {
+            let query = sql.deletes("table").where({ id: 3 });
+            assert.strictEqual(query.Text, "DELETE FROM table WHERE id = $1");
+            assert.deepStrictEqual(query.Values, [3]);
         });
     });
 });
